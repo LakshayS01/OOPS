109cb278890f7d06293caf037ab2cb650501598a
LakshayS01 <lakshaysaini3987@gmail.com>
2025-07-03 15:28:56 +0530

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,42 @@
+// #include<iostream>
+// using namespace std;
+// int main ()
+// {
+//     cout <<"Hellon\nWorld";
+//     return 0;
+// }
+
+//even odd
+// #include <iostream>
+// using namespace std;
+// int main ()
+// {
+//     int n;
+//     cin>>n;
+//     if (n%2==0) cout<<"Even";
+//     else cout<<"Odd";
+//     return 0;
+// }
+
+// int main()
+// {
+//     int n,count=0;
+//     cout<< "Enter a number:";
+//     cin>>n;
+//     for(int i=2;i<n;i++)
+//     {
+//         if(n%i==0)
+//         {
+//             count++;
+//         }
+//     }
+//     if(count==0)
+//     {
+//         cout<<"Prime Number";
+//     }
+//     else
+//     {
+//         cout<<"Not a prime number";
+//     }
+//     return 0;
+// }
